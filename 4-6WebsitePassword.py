fb32814f8349f2400de246cabdeb67c659283e02
swifty-hh <106498918+swifty-hh@users.noreply.github.com>
2023-11-14 22:22:25 +0900

    nadocoding 4-6
    Website Password Generator

---
+++ 4-6WebsitePassword.py
@@ -0,0 +1,7 @@
+url = "http://Apple.com"
+my_str = url.replace("http://", "")
+print(my_str)
+my_str = my_str[:my_str.index(".")]
+print(my_str)
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print("{0}의 비밀번호는 {1} 입니다.".format(url, password))
